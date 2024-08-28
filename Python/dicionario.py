8df78dbf2be74ba53bb4d0bb106339ef10a1b501
MichelGehlen <160542302+MichelGehlen@users.noreply.github.com>
2024-08-27 21:26:47 -0300

    Add files via upload

---
+++ Python/dicionario.py
@@ -0,0 +1,13 @@
+player = {
+    "nome": "João",
+    "level": 1,
+    "hp":100,
+    "exp": 0,
+    "dano": 5,
+}
+
+npcs = [
+    { "nome": "Monstro", "dano": 2, "hp":50, "exp": 5},
+    { "nome": "chefão", "dano":10, "hp":100, "exp":5},
+]
+print(npcs[0]['dano'])
---
+++ Python/if_else.py
@@ -0,0 +1,6 @@
+
+idade = int(input("Digite sua idade: "))
+if idade >= 18:
+     print("Permitido")
+else:
+     print("Não entra")
